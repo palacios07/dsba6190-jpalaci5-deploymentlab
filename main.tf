bf5824182ef70874fa24171a034b00ec52eaf526
palacios07 <jpalaci5@uncc.edu>
2023-04-26 10:32:44 -0400

    update the main with the changes

--- main.tf
+++ main.tf
@@ -42,3 +42,118 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
+resource "azurerm_application_insights" "example" {
+  name                = "workspace-example-ai-jpalaci5"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "example" {
+  name                = "jpalaci5-workspacevault"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_subscription.current.tenant_id
+  sku_name            = "premium"
+}
+
+
+resource "azurerm_machine_learning_workspace" "example" {
+  name                    = "machine-learning-ws-jpalaci5"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.example.id
+  key_vault_id            = azurerm_key_vault.example.id
+  storage_account_id      = azurerm_storage_account.storage.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "tfex-cosmos-db-${random_integer.deployment_id_suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "westus"
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_app_service_plan" "webapp_plan" {
+  name                = "webapp-plan-jpalaci5"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "webapp" {
+  name                = "jpalaci5-webapp-new"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.webapp_plan.id
+}
+
+output "webapp_url" {
+  value = azurerm_app_service.webapp.default_site_hostname
+}
+
+
+resource "azurerm_app_service_plan" "functionapp" {
+  name                = "jpalaci5-functions-service-plan-1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "functionapp" {
+  name                       = "jpalaci5-functions"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.functionapp.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+}
+
